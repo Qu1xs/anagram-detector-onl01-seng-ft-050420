b8fbf14c548bda066ce4120c9196b8e734766f7a
kevin murphy <kevinnealmurphy@gmail.com>
2020-05-10 14:28:37 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,16 +1,13 @@
 # Your code goes here!
 class Anagram
-  attr_accessor :word, :element
-  
+  attr_accessor :word, :list
   
   def initialize(word)
     @word = word
   end
   
   def match(list)
-    array_of_words.select do |element|
-      (@word.split("").sort) == (element.split("").sort)
-    end
+    list.select {|index| @word.split.sort == index.split.sort} 
   end
   
 end
