7972910f2f711452690dcfea7bd0e260439a80cc
kevin murphy <kevinnealmurphy@gmail.com>
2020-05-10 14:29:13 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -13,5 +13,5 @@ class Anagram
 end
 
 
-  listen = Anagram.new("listen")
-  listen.match(%w(enlists google inlets banana))
+#  listen = Anagram.new("listen")
+#  listen.match(%w(enlists google inlets banana))
